c0bfa53e231cca91a5a1d0d0f7a71e60b48bdc90
RealLionelMuhire <RealLionel>
2024-10-10 20:47:16 +0200

    creating education and experience

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -11,10 +11,11 @@ const Qualification = () => {
   return (
     <section className="qualification section">
       <h2 className="section__title">Qualification</h2>
-      <span className="section__subtitle">My personel journey</span>
+      <span className="section__subtitle">My personal journey</span>
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
+          {/* Experience Tab */}
           <div
             className={
               toggleState === 1
@@ -23,10 +24,11 @@ const Qualification = () => {
             }
             onClick={() => toggleTab(1)}
           >
-            <i className="uil uil-graduation-cap qualification__icon"></i>
-            Education
+            <i className="uil uil-briefcase-alt qualification__icon qualification__title-experience"></i>
+            Experience
           </div>
 
+          {/* Education Tab */}
           <div
             className={
               toggleState === 2
@@ -35,12 +37,13 @@ const Qualification = () => {
             }
             onClick={() => toggleTab(2)}
           >
-            <i className="uil uil-briefcase-alt qualification__icon"></i>
-            Experience
+            <i className="uil uil-graduation-cap qualification__icon qualification__title-education"></i>
+            Education
           </div>
         </div>
 
         <div className="qualification__sections">
+          {/* Experience Content */}
           <div
             className={
               toggleState === 1
@@ -50,15 +53,14 @@ const Qualification = () => {
           >
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">Web Design</h3>
+                <h3 className="qualification__title">Software Developer & IT Helpdesk Tech</h3>
                 <span className="qualification__subtitle">
-                  Spain - Institute
+                  ML Corporate Services - Rwanda
                 </span>
                 <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                  <i className="uil uil-calendar-alt"></i> Dec 2023 - Present
                 </div>
               </div>
-
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
@@ -67,34 +69,33 @@ const Qualification = () => {
 
             <div className="qualification__data">
               <div></div>
-
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
-
               <div>
-                <h3 className="qualification__title">Art Director</h3>
+                <h3 className="qualification__title">Software Developer Intern</h3>
                 <span className="qualification__subtitle">
-                  Spain - Institute
+                ML Corporate Services - Rwanda
                 </span>
                 <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2020 - 2021
+                  <i className="uil uil-calendar-alt"></i> Jul 2022 - May 2023
                 </div>
+                
               </div>
             </div>
 
+            
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">Web Development</h3>
+                <h3 className="qualification__title">Medical Imaging Officer</h3>
                 <span className="qualification__subtitle">
-                  Spain - Institute
+                NYARUGENGE District Hospital - Rwanda
                 </span>
                 <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2018 - 2020
+                  <i className="uil uil-calendar-alt"></i> Jun 2023 - Sep 2023
                 </div>
               </div>
-
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
@@ -103,23 +104,23 @@ const Qualification = () => {
 
             <div className="qualification__data">
               <div></div>
-
               <div>
                 <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
               </div>
-
               <div>
-                <h3 className="qualification__title">UX Expert</h3>
+                <h3 className="qualification__title">Software Developer Intern</h3>
                 <span className="qualification__subtitle">
-                  Spain - Institute
+                ALX Africa - Rwanda
                 </span>
                 <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2017 - 2018
+                  <i className="uil uil-calendar-alt"></i> Jun 2023 - Sep 2023
                 </div>
               </div>
             </div>
           </div>
 
+          {/* Education Content */}
           <div
             className={
               toggleState === 2
@@ -128,54 +129,73 @@ const Qualification = () => {
             }
           >
             <div className="qualification__data">
+              <div></div>
               <div>
-                <h3 className="qualification__title">Product Designer</h3>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">AWS Cloud Computing</h3>
                 <span className="qualification__subtitle">
-                  Microsoft - Spain
+                  ALX Africa
                 </span>
                 <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                  <i className="uil uil-calendar-alt"></i> Aug 2024 - Present
                 </div>
               </div>
+            </div>
 
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Full Stack Open Course</h3>
+                <span className="qualification__subtitle">
+                  University of Helsinki
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2023 - 2024
+                </div>
+              </div>
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
             </div>
+            
 
             <div className="qualification__data">
               <div></div>
-
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
-
               <div>
-                <h3 className="qualification__title">UX Designer</h3>
+                <h3 className="qualification__title">ALX Software Engineering Program</h3>
                 <span className="qualification__subtitle">
-                  Apple Inc - Spain
+                  ALX Africa
                 </span>
                 <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2020 - 2021
+                  <i className="uil uil-calendar-alt"></i> Jan 2023 - May 2024
                 </div>
               </div>
             </div>
 
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">Web Designer</h3>
-                <span className="qualification__subtitle">Figma - Spain</span>
+                <h3 className="qualification__title">Bachelor's in Medical Imaging Sciences</h3>
+                <span className="qualification__subtitle">
+                  University of Rwanda
+                </span>
                 <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2018 - 2020
+                  <i className="uil uil-calendar-alt"></i> Oct 2017 - Nov 2022
                 </div>
               </div>
-
               <div>
                 <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
               </div>
             </div>
+
+            
           </div>
         </div>
       </div>
